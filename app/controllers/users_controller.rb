f678e78acf1361d6b10319378b2b4bf7647f1b9a
Wen Li <wenlilearn@gmail.com>
2020-02-18 23:33:18 -0500

    UserStock model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def my_portfolio
+    @stocks = current_user.stocks
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
   def self.lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex[:publishable_token],
