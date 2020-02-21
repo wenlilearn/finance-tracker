3445abe541ffb1ec0e1066b440a750ce447170fd
Wen Li <wenlilearn@gmail.com>
2020-02-20 22:11:27 -0500

    Allow accounts to update name

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,12 @@ class User < ApplicationRecord
   def can_track_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_tracked?(ticker_symbol)
   end
+
+  def full_name
+    if first_name || last_name
+      "#{first_name} #{last_name}"
+    else
+      "Anonymous"
+    end
+  end
 end
